93ff2360d11b59268ed0b27540d6f009776759a8
moguchev <abracadabra.14@mail.ru>
2020-04-03 21:35:07 +0300

    fixes style

--- Lab02/exec.cpp
+++ Lab02/exec.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int main(int argc, int *argv[]) {
+int main(int argc, char** argv) {
     printf("Будет выполнена программа %s...\n\n", argv[0]);
     printf("Выполняется %s", argv[0]);
     execl("hello"," ","Hello", "World!", NULL);
